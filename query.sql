6a86dcbbc3516dd6d7c4939efd8321825c2a7a63
Pranav More (Ext) <pranav.more@tietoevry.com>
2026-03-24 17:20:17 +0530

    thirdh

---
+++ query.sql
@@ -0,0 +1,10 @@
+Create Database MyFirst;
+use MyFirst;
+Create Table Emp(
+ID INT Primary key,
+Name Varchar(50),
+Salary INT
+);
+Insert into Emp Values (1,'Pranav',25000);
+Insert into Emp Values (3, 'rutuj',80000);
+Select * From Emp ;
